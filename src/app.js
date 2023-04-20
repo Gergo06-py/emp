923130bb2ebc12e29c92f782449cded3683b2c3d
Borbély Gergő Árpád <borbelygergo06@gmail.com>
2023-04-20 10:39:15 +0200

    CRUD műveletek működnek

--- src/app.js
+++ src/app.js
@@ -1,24 +1,64 @@
 const doc = {
-  tbody: null,
+  tbody: document.querySelector("#tbody"),
+  saveButton: document.querySelector("#saveButton"),
+  idInput: document.querySelector("#id"),
+  nameInput: document.querySelector("#name"),
+  cityInput: document.querySelector("#city"),
+  salaryInput: document.querySelector("#salary"),
+  operationModalLabel: document.querySelector("#operationModalLabel"),
 };
 const state = {
   dolgozoLista: [],
   host: null,
-  saveButton: null,
+  adding: true
 };
 
 window.addEventListener("load", () => {
   init();
 });
 
-async function init() {
+function init() {
   state.host = "http://localhost:8000/";
-  doc.tbody = document.querySelector("#tbody");
-  doc.saveButton = document.querySelector("#saveButton");
   doc.saveButton.addEventListener("click", () => {
-    console.log("működik");
+    if (state.adding)
+      startAddEmployee();
+    else
+      startUpdate();
   });
   getEmployee();
+}
+
+function startAddEmployee() {
+  let name = doc.nameInput.value;
+  let city = doc.cityInput.value;
+  let salary = Number(doc.salaryInput.value);
+  let employee = {
+    name: name,
+    city: city,
+    salary: salary,
+  };
+  addEmployee(employee);
+}
+
+function addEmployee(employee) {
+  let endpoint = "employees";
+  let url = state.host + endpoint;
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify(employee),
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+      clearModalFields();
+    });
+}
+
+function clearModalFields() {
+  doc.nameInput.value = "";
+  doc.cityInput.value = "";
+  doc.salaryInput.value = "";
 }
 
 function getEmployee() {
@@ -35,7 +75,7 @@ function getEmployee() {
 
 function deleteEmployee(id) {
   let endpoint = "employees";
-  let url = state.host + endpoint + "/" + id;
+  let url = state.host + endpoint + id;
   fetch(url, { method: "DELETE" })
     .then((response) => response.json())
     .then((result) => {
@@ -43,6 +83,51 @@ function deleteEmployee(id) {
       getEmployee();
       render();
     });
+}
+
+function startEditEmployee(event) {
+  state.adding = false;
+  let id = event.getAttribute("data-id");
+  let name = event.getAttribute("data-name");
+  let city = event.getAttribute("data-city");
+  let salary = event.getAttribute("data-salary");
+  // console.log(id, name);
+  doc.idInput.value = id;
+  doc.nameInput.value = name;
+  doc.cityInput.value = city;
+  doc.salaryInput.value = salary;
+  doc.operationModalLabel.textContent = "Szerkesztés";
+}
+
+function startUpdate() {
+  let id = doc.idInput.value;
+  let name = doc.nameInput.value;
+  let city = doc.cityInput.value;
+  let salary = doc.salaryInput.value;
+  let employee = {
+    id: id,
+    name: name,
+    city: city,
+    salary: salary,
+  };
+  updateEmployee(employee);
+  state.adding = true;
+}
+
+function updateEmployee(employee) {
+  let endpoint = 'employees';
+  let url = state.host + endpoint + '/' + employee.id;
+  fetch(url, {
+    method: 'PUT',
+    body: JSON.stringify(employee),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => response.json())
+  .then(result => {
+    console.log(result);
+  });
 }
 
 function startDeleteEmployee(event) {
@@ -60,6 +145,20 @@ function render() {
         <td>${dolgozo.name}</td>
         <td>${dolgozo.city}</td>
         <td>${dolgozo.salary}</td>
+        <td>
+          <button
+            class="btn btn-warning"
+            onclick="startEditEmployee(this)"
+            data-id="${dolgozo.id}"
+            data-name="${dolgozo.name}"
+            data-city="${dolgozo.city}"
+            data-salary="${dolgozo.salary}"
+            data-bs-toggle="modal"
+            data-bs-target="#operationModal"
+          >
+            Szerkesztés
+          </button>
+        </td>
         <td>
           <button
             class="btn btn-danger"
